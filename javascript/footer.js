da7dec1df818ac5f9c7eda8b14901c8878abbe33
boveus <boveus@gmail.com>
2019-01-27 18:07:22 -0500

    implement the correct link behavior

--- javascript/footer.js
+++ javascript/footer.js
@@ -5,6 +5,6 @@ var render = function (template, node) {
 var linkedInUrl = "<a class='fa fa-linkedin footerbutton mr-2' href='https://www.linkedin.com/in/brandon-scott-stewart/'>";
 var githubUrl = "<a class='fa fa-github footerbutton mr-2' href='https://github.com/boveus'>";
 var email = "<a class='fa fa-envelope footerbutton mr-2' href='mailto:brandonsstewart88@gmail.com'>";
-var footerTemplate = "<div class='mt-4'>" + linkedInUrl + githubUrl + email + "</div>";
+var footerTemplate = linkedInUrl + githubUrl + email;
 
 render(footerTemplate, document.querySelector('#footer'));
